f24b13119269e175c182dc74d6ae6789bcbc85a6
LatishSehgal <latish.sehgal@gmail.com>
2012-07-18 13:34:04 -0500

    Initial commit

---
+++ BevelEffect/BevelBorderUserControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BevelEffect
+{
+    public partial class BevelBorderUserControl : UserControl
+    {
+        public BevelBorderUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius) GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public Thickness BevelBorderThickness
+        {
+            get { return (Thickness) GetValue(BevelBorderThicknessProperty); }
+            set { SetValue(BevelBorderThicknessProperty, value); }
+        }
+
+        public Brush FirstHalfBrush
+        {
+            get { return (Brush) GetValue(FirstHalfBrushProperty); }
+            set { SetValue(FirstHalfBrushProperty, value); }
+        }
+
+        public Brush SecondHalfBrush
+        {
+            get { return (Brush) GetValue(SecondHalfBrushProperty); }
+            set { SetValue(SecondHalfBrushProperty, value); }
+        }
+
+        public Brush BackgroundFill
+        {
+            get { return (Brush) GetValue(BackgroundFillProperty); }
+            set { SetValue(BackgroundFillProperty, value); }
+        }
+
+        static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            "CornerRadius", typeof(CornerRadius), typeof(BevelBorderUserControl));
+        static readonly DependencyProperty BevelBorderThicknessProperty = DependencyProperty.Register(
+            "BevelBorderThickness", typeof(Thickness), typeof(BevelBorderUserControl));
+
+
+        static readonly DependencyProperty FirstHalfBrushProperty = DependencyProperty.Register(
+            "FirstHalfBrush", typeof(Brush), typeof(BevelBorderUserControl));
+
+        static readonly DependencyProperty SecondHalfBrushProperty = DependencyProperty.Register(
+            "SecondHalfBrush", typeof(Brush), typeof(BevelBorderUserControl));
+
+        static readonly DependencyProperty BackgroundFillProperty = DependencyProperty.Register(
+            "BackgroundFillProperty", typeof(Brush), typeof(BevelBorderUserControl));
+    }
+}
---
+++ BevelEffect/PolygonPointsConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BevelEffect
+{
+    public class PolygonPointsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var polygonPoints = new PointCollection();
+            if (values.Length == 3 && values[2] is Thickness)
+            {
+                var width = System.Convert.ToDouble(values[0]);
+                var height = System.Convert.ToDouble(values[1]);
+                var margin = (Thickness)values[2];
+
+                if (System.Convert.ToInt32(parameter) == 0)
+                {
+                    polygonPoints.Add(new Point(0 + margin.Left, height - margin.Bottom));
+                    polygonPoints.Add(new Point(0, height));
+                    polygonPoints.Add(new Point(0, 0));
+                    polygonPoints.Add(new Point(width, 0));
+                    polygonPoints.Add(new Point(width - margin.Right, 0 + margin.Top));
+                }
+                else
+                {
+                    polygonPoints.Add(new Point(0 + margin.Left, height - margin.Bottom));
+                    polygonPoints.Add(new Point(0, height));
+                    polygonPoints.Add(new Point(width, height));
+                    polygonPoints.Add(new Point(width, 0));
+                    polygonPoints.Add(new Point(width - margin.Right, 0 + margin.Top));
+                }
+            }
+            return polygonPoints;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
